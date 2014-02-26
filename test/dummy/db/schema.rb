348901850ac347b4643699e5c9931d06b6f35555
Abdulaziz AlShetwi <ecleeld@gmail.com>
2014-02-26 21:29:07 +0300

    update version.

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111151630) do
+ActiveRecord::Schema.define(version: 20140226181159) do
 
   create_table "ads_ads", force: true do |t|
     t.integer  "campaign_id"
