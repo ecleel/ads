51016430078347a014cfb63bc358028f4774a9be
Abdulaziz AlShetwi <ecleeld@gmail.com>
2014-02-26 21:26:27 +0300

    Precompile assets with host app.
    Change dummy production config to serve assets and not fallback to asset
    pipeline if it's missed.

--- lib/ads/engine.rb
+++ lib/ads/engine.rb
@@ -5,5 +5,11 @@ module Ads
     initializer 'ads.locale.load' do |app|
       app.config.i18n.load_path += Dir[File.join(Ads::Engine.root, 'config', 'locales', '**', '*.{rb,yml')]
     end
+    
+    if Rails.version >= '3.1'
+      initializer :assets do |config|
+        Rails.application.config.assets.precompile += %w( ads/application.js ads.js ads/application.css )
+      end
+    end
   end
 end
