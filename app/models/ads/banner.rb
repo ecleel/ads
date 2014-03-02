a53c6d88fe5a98ffc6bfafcb9857f1a6f2ddaccb
Abdulaziz AlShetwi <ecleeld@gmail.com>
2014-03-02 14:57:06 +0300

    split lines more than 80 characters into multiple lines.

--- app/models/ads/banner.rb
+++ app/models/ads/banner.rb
@@ -3,7 +3,7 @@ module Ads
     has_many :ads
 
     # BANNER = all.each_with_object({}) {|banner, hsh| hsh[banner.name] = banner}
-    # TODO validate name shouldn't have spaces just letters and underscore.
+
     validates :name,   presence: true, uniqueness: true
     validates :width,  presence: true
     validates :height, presence: true
